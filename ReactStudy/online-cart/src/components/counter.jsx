a09629f2c45be860947c0135a3f20ca755da6581
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2021-08-25 12:56:49 +0530

    Update

--- ReactStudy/online-cart/src/components/counter.jsx
+++ ReactStudy/online-cart/src/components/counter.jsx
@@ -1,59 +1,47 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
   styles = {
     fontSize: 14,
     fontWeight: "bold",
+    width: 60,
+    padding: 12,
   };
 
   render() {
     return (
       <React.Fragment>
         <div>
+          <span>{this.props.children}</span>
           <span style={this.styles} className={this.getBadgeClasses()}>
             {this.formatCount()}
           </span>
           <button
             className="btn btn-secondary"
-            onClick={() => this.incrementHandle({ id: 1 })}
+            onClick={() => this.props.onIncrement(this.props.counter)}
           >
-            increment
+            Increment
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+          >
+            Delete
           </button>
         </div>
       </React.Fragment>
     );
   }
 
-  incrementHandle = (product) => {
-    console.log("IncrementHandle called, Count: ", this.state.count);
-    console.log("product: ", product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- ReactStudy/online-cart/src/components/counters.jsx
+++ ReactStudy/online-cart/src/components/counters.jsx
@@ -5,21 +5,44 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 0 },
+      { id: 2, value: 4 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
       { id: 5, value: 0 },
     ],
   };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onIncrement={this.incrementHandle}
+            onDelete={this.deleteHandle}
+          >
+            <h5>Counter #{counter.id}</h5>
+          </Counter>
         ))}
       </div>
     );
   }
+
+  incrementHandle = (counter) => {
+    console.log("IncrementHandle called, counter: ", counter);
+    const counters = this.state.counters;
+    const index = counters.indexOf(counter);
+    counters[index] = counter;
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  deleteHandle = (counterId) => {
+    console.log("deleteHandle Called, counterId: " + counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
 }
 
 export default Counters;
