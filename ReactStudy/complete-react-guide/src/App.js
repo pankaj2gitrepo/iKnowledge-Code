5361d0de1984a79dc08c9e243cb5e753a9656ec0
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2021-10-22 12:55:26 +0530

    Update

--- ReactStudy/complete-react-guide/src/App.js
+++ ReactStudy/complete-react-guide/src/App.js
@@ -1,7 +1,30 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expenses = [
+    { title: "Paper", amount: 11.2, date: new Date(2021, 2, 11) },
+    { title: "Pencil", amount: 15.2, date: new Date(2021, 2, 15) },
+    { title: "Eraser", amount: 18.2, date: new Date(2021, 2, 18) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
