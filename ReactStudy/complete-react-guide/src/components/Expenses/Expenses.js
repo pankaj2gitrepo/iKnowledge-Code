59d7a60e02800955b65007063001d2b591f85556
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2021-10-22 19:36:12 +0530

    Organize compoments

--- ReactStudy/complete-react-guide/src/components/Expenses/Expenses.js
+++ ReactStudy/complete-react-guide/src/components/Expenses/Expenses.js
@@ -1,11 +1,12 @@
 import "./Expenses.css";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   const expenses = props.expenses;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -21,7 +22,7 @@ function Expenses(props) {
         amount={expenses[2].amount}
         date={expenses[2].date}
       />
-    </div>
+    </Card>
   );
 }
 
