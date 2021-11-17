b614ff8cf546e8cb9c34ff829f8599ee45317d0b
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2021-11-17 21:01:49 +0530

    Add

--- ReactStudy/complete-react-guide/src/App.js
+++ ReactStudy/complete-react-guide/src/App.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, title: "Paper", amount: 11.2, date: new Date(2021, 2, 11) },
     { id: 2, title: "Pencil", amount: 15.2, date: new Date(2021, 2, 15) },
     { id: 3, title: "Eraser", amount: 18.2, date: new Date(2021, 2, 18) },
-  ];
+  ]);
 
   const addExpenseHandler = (expenseDataAdded) => {
-    console.log(`App.js expenseDataAdded`);
+    console.log(`App.js expenseDataAdded:${JSON.stringify(expenseDataAdded)}`);
+    setExpenses([...expenses, expenseDataAdded]);
   };
 
   return (
