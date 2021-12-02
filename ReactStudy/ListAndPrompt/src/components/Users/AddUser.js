7ea617684fc300d7c726aac5a15109dcdea1ceef
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2021-12-02 12:26:50 +0530

    Update

--- ReactStudy/ListAndPrompt/src/components/Users/AddUser.js
+++ ReactStudy/ListAndPrompt/src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React from "react";
 
+import Card from "../UI/Card";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
