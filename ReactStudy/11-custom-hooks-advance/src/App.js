85fcd92a09e1b7ff8dade346c41ae31f3a0d0707
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2022-01-02 15:13:13 +0530

    Hook updated

--- ReactStudy/11-custom-hooks-advance/src/App.js
+++ ReactStudy/11-custom-hooks-advance/src/App.js
@@ -6,28 +6,23 @@ import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://react-http-f2bfb-default-rtdb.firebaseio.com/tasks.json",
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-f2bfb-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
   }, []);
 
   const taskAddHandler = (task) => {
