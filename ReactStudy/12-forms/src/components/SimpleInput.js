dc8df49d226c28296d2bdb6788695716f90d6588
Pankaj Sonawane <pankaj.gitrepo@gmail.com>
2022-01-04 15:55:28 +0530

    Form Input validation

--- ReactStudy/12-forms/src/components/SimpleInput.js
+++ ReactStudy/12-forms/src/components/SimpleInput.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [inputName, setInputName] = useState("");
+  const [isInputNameValid, setIsInputNameValid] = useState(false);
+  const [isInputNameTouched, setIsInputNameTouched] = useState(false);
+  const nameInputIsInvalid = isInputNameTouched && !isInputNameValid;
+
+  const inputNameChangeHandler = (event) => {
+    setInputName(event.target.value);
+    console.log(inputName);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setIsInputNameTouched(true);
+
+    if (inputName.trim() === "") {
+      setIsInputNameValid(false);
+      return;
+    }
+    setIsInputNameValid(true);
+    console.log(inputName);
+    setInputName("");
+  };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={inputNameChangeHandler}
+          value={inputName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty!</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
