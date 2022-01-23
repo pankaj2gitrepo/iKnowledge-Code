cd43b92ecb93f6d230b26d40f420a6a08d98a650
Pankaj.Sonawane <pankaj.gitrepo@gmail.com>
2022-01-23 16:25:08 +0530

    Counter

--- ReactStudy/15-redux-01/src/components/Counter.js
+++ ReactStudy/15-redux-01/src/components/Counter.js
@@ -1,12 +1,24 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementCounterHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementCounterHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <button onClick={incrementCounterHandler}>Increment Counter</button>
+      <button onClick={decrementCounterHandler}>Decrement Counter</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
